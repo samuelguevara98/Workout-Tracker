358e2bcba9e3d10db94a6e12b455872bc2084237
Samuel Guevara <samsjamin@outlook.com>
2020-06-27 22:28:47 -0400

    Initialized mongodb

--- server.js
+++ server.js
@@ -2,4 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 var path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000
 
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.listen(PORT, () =>{
+    console.log(`App running on port ${PORT}!`);
+});
