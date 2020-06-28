e9f6871e4594fa3655f96a688d4f6973ef8f8bd2
Samuel Guevara <samsjamin@outlook.com>
2020-06-27 22:49:12 -0400

    Set up mongoose

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 var path = require("path");
+const { connect } = require("http2");
+const { userInfo } = require("os");
 const app = express();
 const PORT = process.env.PORT || 3000
+const db = mongoose.connection;
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.listen(PORT, () =>{
-    console.log(`App running on port ${PORT}!`);
+//db code
+db.on("error", error => {
+    console.log("Encountered a database error!", error);
+});
+
+mongoose/connect(process.env/MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}!`)
 });
