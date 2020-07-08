c163a639da10ae24b9a064ba526e4cf2add6d77a
Samuel Guevara <samsjamin@outlook.com>
2020-07-07 21:00:27 -0400

    updated routes

---
+++ routes/html.js
@@ -0,0 +1,16 @@
+var path = require("path");
+const router = require("express").Router();
+
+router.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "..//public/index.html"));
+});
+
+router.get("/exercise", function (req, res){
+    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+});
+
+router.get("stats", function (req, res){
+    res.sendFile(path.join(_dirname, "../public/stats.html"));
+});
+
+module.exports = router;
